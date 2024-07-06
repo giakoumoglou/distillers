95e1e38594ddbeba1a4555c7da9a28cee1d20b0a
Nick Giakoumoglou <n.giakoumoglou23@imperial.ac.uk>
2024-07-06 22:08:52 +0300

    Update run_cifar_distill.sh

--- scripts/run_cifar_distill.sh
+++ scripts/run_cifar_distill.sh
@@ -1,7 +1,7 @@
 # sample scripts for running the distillation code
 # use resnet32x4 and resnet8x4 as an example
 
-# kd
+# KD
 python train_student.py --path_t ./save/models/resnet32x4_vanilla/ckpt_epoch_240.pth --distill kd --model_s resnet8x4 -r 0.1 -a 0.9 -b 0 --trial 1
 # FitNet
 python train_student.py --path_t ./save/models/resnet32x4_vanilla/ckpt_epoch_240.pth --distill hint --model_s resnet8x4 -a 0 -b 100 --trial 1
