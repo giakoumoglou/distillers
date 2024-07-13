3931463982f795fb719e1a189a12b64962d1cd06
Nick Giakoumoglou <n.giakoumoglou23@imperial.ac.uk>
2024-07-13 14:39:35 +0300

    Add files via upload

--- transfer_student.py
+++ transfer_student.py
@@ -5,180 +5,259 @@
 # LICENSE file in the root directory of this source tree.
 
 from __future__ import print_function
+import os
 import argparse
+import sys
 import time
 import numpy as np
-import matplotlib.pyplot as plt
 import torch
+import torch.optim as optim
 import torch.nn as nn
+import torch.backends.cudnn as cudnn
+from torch.utils.tensorboard import SummaryWriter
 
 from models import model_dict
-from datasets import get_cifar100_dataloaders, get_cifar10_dataloaders
+from dataset.stl10 import get_stl10_dataloaders
+from dataset.tinyimagenet200 import get_tiny_imagenet_dataloaders
 
 
 def parse_option():
 
-    parser = argparse.ArgumentParser('argument for training')
-    
+    parser = argparse.ArgumentParser('PyTorch Knowledge Distillation - Transfer to STL-10/TIN-200')
+
     parser.add_argument('--print_freq', type=int, default=100, help='print frequency')
-    parser.add_argument('--eval', action='store_true', help='evaluate student and teacher')
-    parser.add_argument('--filename', type=str, default='fig', help='name of figure')
-    parser.add_argument('--plot_multiple', action='store_true', help='plot multiple images')
+    parser.add_argument('--tb_freq', type=int, default=500, help='tb frequency')
+    parser.add_argument('--save_freq', type=int, default=50, help='save frequency')
+    parser.add_argument('--batch_size', type=int, default=64, help='batch_size')
+    parser.add_argument('--num_workers', type=int, default=8, help='num of workers to use')
+    parser.add_argument('--epochs', type=int, default=100, help='number of training epochs')
+
+    # optimization
+    parser.add_argument('--learning_rate', type=float, default=0.05, help='learning rate')
+    parser.add_argument('--lr_decay_epochs', type=str, default='150,180,210', help='where to decay lr, can be a list')
+    parser.add_argument('--lr_decay_rate', type=float, default=0.1, help='decay rate for learning rate')
+    parser.add_argument('--weight_decay', type=float, default=5e-4, help='weight decay')
+    parser.add_argument('--momentum', type=float, default=0.9, help='momentum')
 
     # dataset
-    parser.add_argument('--dataset', type=str, default='cifar100', choices=['cifar100'], help='dataset')
-
-    # model
     parser.add_argument('--model_s', type=str, default='resnet8', choices=['resnet8', 'resnet14', 'resnet20', 'resnet32', 'resnet44', 'resnet56', 'resnet110', 'resnet8x4', 'resnet32x4', 'wrn_16_1', 'wrn_16_2', 'wrn_40_1', 'wrn_40_2', 'vgg8', 'vgg11', 'vgg13', 'vgg16', 'vgg19', 'ResNet50', 'MobileNetV2', 'ShuffleV1', 'ShuffleV2'])
     parser.add_argument('--path_s', type=str, default=None, help='student model snapshot')
-    parser.add_argument('--path_t', type=str, default=None, help='teacher model snapshot')
+    parser.add_argument('--dataset', type=str, default='stl10', choices=['stl10', 'tinyimagenet'], help='dataset')
 
-    opt = parser.parse_args()    
+    parser.add_argument('-t', '--trial', type=int, default=0, help='the experiment id')
+
+    opt = parser.parse_args()
+    
+    opt.model_s = 'wrn_16_2'
+    opt.path_s = './save//wrn_16_2_last.pth'
+    
+    # set different learning rate from these 4 models
+    if opt.model_s in ['MobileNetV2', 'ShuffleV1', 'ShuffleV2']:
+        opt.learning_rate = 0.01
+
+    # set the path according to the environment
+    opt.model_path = './save/ft/models'
+    opt.tb_path = './save/ft/tensorboard'
+
+    iterations = opt.lr_decay_epochs.split(',')
+    opt.lr_decay_epochs = list([])
+    for it in iterations:
+        opt.lr_decay_epochs.append(int(it))
+
+    opt.model_name = '{}_{}_lr_{}_decay_{}_trial_{}'.format(opt.model_s, opt.dataset, opt.learning_rate,
+                                                            opt.weight_decay, opt.trial)
+
+    opt.tb_folder = os.path.join(opt.tb_path, opt.model_name)
+    if not os.path.isdir(opt.tb_folder):
+        os.makedirs(opt.tb_folder)
+
+    opt.save_folder = os.path.join(opt.model_path, opt.model_name)
+    if not os.path.isdir(opt.save_folder):
+        os.makedirs(opt.save_folder)
+
     return opt
 
 
-def get_teacher_name(model_path):
-    """ Parse teacher name """
-    segments = model_path.split('/')[-2].split('_')
-    if segments[0] != 'wrn':
-        return segments[0]
-    else:
-        return segments[0] + '_' + segments[1] + '_' + segments[2]
-
-
-def load_teacher(model_path, n_cls):
-    """ Load teacher model """
-    print('==> Loading teacher model')
-    model_t = get_teacher_name(model_path)
-    model = model_dict[model_t](num_classes=n_cls)
-    try:
-        model.load_state_dict(torch.load(model_path)['model'])
-    except:
-        model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu'))['model'])
-    print('Teacher model loaded')
-    return model
-
 def load_student(model_path, n_cls, model_s):
-    """ Load student model """
     print('==> Loading student model')
     model = model_dict[model_s](num_classes=n_cls)
     try:
-        model.load_state_dict(torch.load(model_path)['model'])
+        state_dict = torch.load(model_path)['model']
     except:
-        model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu'))['model'])
+        state_dict = torch.load(model_path, map_location=torch.device('cpu'))['model']
+    msg = model.load_state_dict(state_dict, strict=False)
+    print(msg)
     print('Student model loaded')
     return model
 
-
 def main():
+    best_acc = 0
 
     opt = parse_option()
 
-    ###########################
-    ####### Data loader #######
-    ###########################
-    if opt.dataset == 'cifar100':
-        train_loader, val_loader = get_cifar100_dataloaders(batch_size=256, num_workers=8, is_instance=False)
-        n_cls = 100
-    elif opt.dataset == 'cifar10':
-        train_loader, val_loader = get_cifar10_dataloaders(batch_size=256, num_workers=8, is_instance=False)
+    # dataloader
+    if opt.dataset == 'stl10':
+        train_loader, val_loader = get_stl10_dataloaders(batch_size=opt.batch_size, num_workers=opt.num_workers)
         n_cls = 10
+        print(f"Length of STL10 training dataset: {len(train_loader.dataset)}")
+        print(f"Length of STL10 validation dataset: {len(val_loader.dataset)}")
+    elif opt.dataset == 'tinyimagenet':
+        train_loader, val_loader = get_tiny_imagenet_dataloaders(batch_size=opt.batch_size, num_workers=opt.num_workers)
+        n_cls = 200
+        print(f"Length of Tiny ImageNet training dataset: {len(train_loader.dataset)}")
+        print(f"Length of Tiny ImageNet validation dataset: {len(val_loader.dataset)}")
     else:
         raise NotImplementedError(opt.dataset)
 
-    ###########################
-    ########## Model ##########
-    ###########################
-    model_t = load_teacher(opt.path_t, n_cls)
-    model_s = load_student(opt.path_s, n_cls, opt.model_s)
+    # model
+    model = load_student(opt.path_s, 100, opt.model_s)
+    model.fc = nn.Linear(model.fc.in_features, n_cls)
+    model.fc.weight.data.normal_(mean=0.0, std=0.01)
+    model.fc.bias.data.zero_()
     
+    # freeze model
+    for name, param in model.named_parameters():
+        if 'fc' in name:
+            param.requires_grad = True
+        else:
+            param.requires_grad = False
+    print("Model modified and all layers except the last `fc` layer are frozen.")
+
+
+    # optimizer
+    optimizer = optim.SGD([param for param in model.parameters() if param.requires_grad],
+                          lr=opt.learning_rate,
+                          momentum=opt.momentum,
+                          weight_decay=opt.weight_decay)
+
+    criterion = nn.CrossEntropyLoss()
+
     if torch.cuda.is_available():
-        model_t.cuda()
-        model_s.cuda()
+        model = model.cuda()
+        criterion = criterion.cuda()
+        cudnn.benchmark = True
 
-    ###########################
-    ######## Mock data ########
-    ###########################
-    data = torch.randn(2, 3, 32, 32)
-    model_t.eval()
-    model_s.eval()
-    
-    ###########################
-    ### Eval teacher/student ##
-    ###########################
-    if opt.eval:
-        teacher_acc, _, _ = validate(val_loader, model_t, nn.CrossEntropyLoss(), opt)
-        print(f'Teacher accuracy: {teacher_acc.item()}%')
-        
-        student_acc, _, _ = validate(val_loader, model_s, nn.CrossEntropyLoss(), opt)
-        print(f'Student accuracy: {student_acc.item()}%')
-    
-    ###########################
-    ######## Correlation #######
-    ###########################
-    if opt.plot_multiple:
-        for i, (data, targets) in enumerate(val_loader):
-            if i >= 30:
-                break
-            with torch.no_grad():
-                data = data.cuda()
-                targets = targets.cuda()
-                
-                logits_t = model_t(data, is_feat=True)[1]
-                logits_s = model_s(data, is_feat=True)[1]
+    # tensorboard
+    logger = SummaryWriter(opt.tb_folder)
 
-                corr_t = correlation_matrix_norm(logits_t)
-                corr_s = correlation_matrix_norm(logits_s)
+    # routine
+    for epoch in range(1, opt.epochs + 1):
 
-                plot_difference(corr_t.detach().cpu().numpy(), corr_s.detach().cpu().numpy(), f'{opt.filename}_{i+1}')
-    else:
-        for data, targets in val_loader:
-            with torch.no_grad():
-                data = data.cuda()
-                targets = targets.cuda()
-                
-                logits_t = model_t(data, is_feat=True)[1]
-                logits_s = model_s(data, is_feat=True)[1]
+        adjust_learning_rate(epoch, opt, optimizer)
+        print("==> training...")
 
-                corr_t = correlation_matrix_norm(logits_t)
-                corr_s = correlation_matrix_norm(logits_s)
-                
-                print(f'Difference: Max: {(corr_t.detach().cpu().numpy() - corr_s.detach().cpu().numpy()).max()}, Min: {(corr_t.detach().cpu().numpy() - corr_s.detach().cpu().numpy()).min()}')
+        time1 = time.time()
+        train_acc, train_loss = train(epoch, train_loader, model, criterion, optimizer, opt)
+        time2 = time.time()
+        print('epoch {}, total time {:.2f}'.format(epoch, time2 - time1))
 
-                plot_difference(corr_t.detach().cpu().numpy(), corr_s.detach().cpu().numpy(), opt.filename)
-                break
+        logger.add_scalar('train_acc', train_acc, epoch)
+        logger.add_scalar('train_loss', train_loss, epoch)
 
+        test_acc, test_acc_top5, test_loss = validate(val_loader, model, criterion, opt)
 
-def correlation_matrix_norm(logits):
-    """ Compute the correlation matrix from logits, ensuring each feature has unit variance """
-    logits_centered = logits - logits.mean(dim=0)
-    logits_normalized = logits_centered / logits_centered.std(dim=0, keepdim=True)
-    correlation = torch.mm(logits_normalized.T, logits_normalized) / logits.size(0)
-    return correlation
+        logger.add_scalar('test_acc', test_acc, epoch)
+        logger.add_scalar('test_acc_top5', test_acc_top5, epoch)
+        logger.add_scalar('test_loss', test_loss, epoch)
 
+        if test_acc > best_acc:
+            best_acc = test_acc
+            state = {
+                'epoch': epoch,
+                'model': model.state_dict(),
+                'best_acc': best_acc,
+                'optimizer': optimizer.state_dict(),
+            }
+            save_file = os.path.join(opt.save_folder, '{}_best.pth'.format(opt.model_s))
+            print('saving the best model!')
+            torch.save(state, save_file)
 
-def correlation_matrix(logits):
-    """ Compute the correlation matrix from logits, ensuring each feature has unit variance """
-    correlation = torch.mm(logits.T, logits) / logits.size(0)
-    return correlation
+        if epoch % opt.save_freq == 0:
+            print('==> Saving...')
+            state = {
+                'epoch': epoch,
+                'model': model.state_dict(),
+                'accuracy': test_acc,
+                'optimizer': optimizer.state_dict(),
+            }
+            save_file = os.path.join(opt.save_folder, 'ckpt_epoch_{epoch}.pth'.format(epoch=epoch))
+            torch.save(state, save_file)
+
+    # This best accuracy is only for printing purpose.
+    # The results reported in the paper/README is from the last epoch.
+    print('==> Best accuracy:', best_acc)
+
+    # save model
+    state = {
+        'opt': opt,
+        'model': model.state_dict(),
+        'optimizer': optimizer.state_dict(),
+    }
+    save_file = os.path.join(opt.save_folder, '{}_last.pth'.format(opt.model_s))
+    torch.save(state, save_file)
+    logger.close()
 
 
-def plot_difference(corr_t, corr_s, filename='fig'):
-    """ Plot the difference in correlation matrices as a heatmap """
-    diff_corr = corr_t - corr_s
-    plt.figure(figsize=(10, 8))
-    im = plt.imshow(diff_corr, cmap='PuOr',)# vmin=-0.3, vmax=0.3)
-    plt.colorbar(im)
-    plt.xticks(np.arange(0, 100, 4))  
-    plt.yticks(np.arange(0, 100, 4))  
-    plt.grid(False) 
-    plt.savefig(f'{filename}.png', dpi=300, format='png', bbox_inches='tight')
-    plt.show()  
-    plt.close()
+def train(epoch, train_loader, model, criterion, optimizer, opt):
+    """Vanilla training"""
+    model.train()
+
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    top5 = AverageMeter()
+
+    end = time.time()
+    for idx, (input, target) in enumerate(train_loader):
+        data_time.update(time.time() - end)
+
+        input = input.float()
+        if torch.cuda.is_available():
+            input = input.cuda()
+            target = target.cuda()
+
+        # ===================forward=====================
+        output = model(input)
+        loss = criterion(output, target)
+
+        acc1, acc5 = accuracy(output, target, topk=(1, 5))
+        losses.update(loss.item(), input.size(0))
+        top1.update(acc1[0], input.size(0))
+        top5.update(acc5[0], input.size(0))
+
+        # ===================backward=====================
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # ===================meters=====================
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        # Tensorboard logger
+        pass
+
+        # print info
+        if idx % opt.print_freq == 0:
+            print('Epoch: [{0}][{1}/{2}]\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                  'Acc@1 {top1.val:.3f} ({top1.avg:.3f})\t'
+                  'Acc@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
+                   epoch, idx, len(train_loader), batch_time=batch_time,
+                   data_time=data_time, loss=losses, top1=top1, top5=top5))
+            sys.stdout.flush()
+
+    print(' * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}'
+          .format(top1=top1, top5=top5))
+
+    return top1.avg, losses.avg
 
 
 def validate(val_loader, model, criterion, opt):
-    """ Validation """
+    """Validation"""
     batch_time = AverageMeter()
     losses = AverageMeter()
     top1 = AverageMeter()
@@ -219,13 +298,14 @@ def validate(val_loader, model, criterion, opt):
                        idx, len(val_loader), batch_time=batch_time, loss=losses,
                        top1=top1, top5=top5))
 
-        print(' * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}'.format(top1=top1, top5=top5))
+        print(' * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}'
+              .format(top1=top1, top5=top5))
 
     return top1.avg, top5.avg, losses.avg
 
 
 class AverageMeter(object):
-    """ Computes and stores the average and current value """
+    """Computes and stores the average and current value"""
     def __init__(self):
         self.reset()
 
@@ -243,7 +323,7 @@ class AverageMeter(object):
 
 
 def accuracy(output, target, topk=(1,)):
-    """ Computes the accuracy over the k top predictions for the specified values of k """
+    """Computes the accuracy over the k top predictions for the specified values of k"""
     with torch.no_grad():
         maxk = max(topk)
         batch_size = target.size(0)
@@ -259,5 +339,20 @@ def accuracy(output, target, topk=(1,)):
         return res
     
     
+def adjust_learning_rate_new(epoch, optimizer, LUT):
+    """Learning rate schedule according to RotNet"""
+    lr = next((lr for (max_epoch, lr) in LUT if max_epoch > epoch), LUT[-1][1])
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+
+def adjust_learning_rate(epoch, opt, optimizer):
+    """Sets the learning rate to the initial LR decayed by decay rate every steep step"""
+    steps = np.sum(epoch > np.asarray(opt.lr_decay_epochs))
+    if steps > 0:
+        new_lr = opt.learning_rate * (opt.lr_decay_rate ** steps)
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = new_lr
+
 if __name__ == '__main__':
     main()
